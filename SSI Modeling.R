e6acef21b619ceb1c09cf2aae8f15e57435747e6
samirsafwan <31454686+samirsafwan@users.noreply.github.com>
2017-11-01 18:28:13 -0700

    Update SSI Modeling.R

--- SSI Modeling.R
+++ SSI Modeling.R
@@ -1,40 +1,72 @@
 setwd("~/Downloads")
-data=read.csv("ssi54-2.csv")
+data=read.csv("ssi54-3.csv")
 
 
-# "time", "altitud_barometer", "lat_gps", "long_gps", "valve_time_total", 
-# "ballast_time_total"
-
-# decision tree approach
-
-# make training and test sets
-set.seed(1)
-s1=sample(5504598, 1000000)
-train1=data[-s1,]
-test1=data[s1,]
-
-library(tree)
+# GBM
 library(gbm)
 set.seed(1)
-tree.fit=tree(altitude_barometer~.-time, data=train1)
-plot(tree.fit)
-text(tree.fit, pretty=0)
-tree.pred=predict(tree.fit, test1)
-mean((tree.pred-test1$altitude_barometer)^2)
+boost.mse=rep(0,19)
+predicted.trajectory=vector()
+for (i in 1:19) {
+  sample=data[1:(1396*i),]
+  test=data[(1396*i+1):(1396*(i+1)),]
+  boost.fit=gbm(altitude~., data=sample, distribution='gaussian', n.trees=2000, shrinkage=0.1, cv.folds=5)
+  boost.pred=predict(boost.fit, test, n.trees=gbm.perf(boost.fit, method='cv'))
+  predicted.trajectory=c(predicted.trajectory, boost.pred)
+  boost.mse[i]=mean((boost.pred-test$altitude)^2)
+}
+boost.mse # mean mse = 414,710
+plot(data$time, data$altitude, type='l')
+lines(data[1397:27920,]$time, predicted.trajectory, type='l', col='red')
+for (i in 1:18) {
+  abline(v=data[1396*i:1396*i,]$time, col='gray')
+}
 
+# Without temp
 library(gbm)
 set.seed(1)
-boost.fit=gbm(altitude_barometer~.-time, data=train1, distribution='gaussian', n.trees=500, shrinkage=0.1)
-boost.pred=predict(boost.fit, test1, n.trees=500)
-mean((boost.pred-test1$altitude_barometer)^2)
+boost.mse2=rep(0,19)
+predicted.trajectory2=vector()
+for (i in 1:19) {
+  sample=data[1:(1396*i),]
+  test=data[(1396*i+1):(1396*(i+1)),]
+  boost.fit=gbm(altitude~.-temperature, data=sample, distribution='gaussian', n.trees=2000, shrinkage=0.1, cv.folds=5)
+  boost.pred=predict(boost.fit, test, n.trees=gbm.perf(boost.fit, method='cv'))
+  predicted.trajectory2=c(predicted.trajectory2, boost.pred)
+  boost.mse2[i]=mean((boost.pred-test$altitude)^2)
+}
+boost.mse2 # mean mse = 3,415,947
+plot(data$time, data$altitude, type='l')
+lines(data[1397:27920,]$time, predicted.trajectory2, type='l', col='red')
+for (i in 1:18) {
+  abline(v=data[1396*i:1396*i,]$time, col='gray')
+}
 
+# Lasso
+library(glmnet)
+set.seed(1)
+lasso.mse=rep(0,19)
+for (i in 1:19) {
+  sampleX=as.matrix(data[1:(1396*i),1:12])
+  sampleY=as.matrix(data[1:(1396*i),]$altitude)
+  testX=as.matrix(data[(1396*i+1):(1396*(i+1)),1:12])
+  lasso.fit=cv.glmnet(sampleX, sampleY, alpha=1)
+  lasso.pred=predict(lasso.fit, s=lasso.fit$lambda.min, newx=testX)
+  lasso.mse[i]=mean((lasso.pred-data[(1396*i+1):(1396*(i+1)),]$altitude)^2)
+}
+lasso.mse # mean mse = 3,415,296
+
+
+# GAM with Splines
 library(splines)
-spline.fit1=smooth.spline(train1$altitude_barometer, train1$lat_gps, cv=TRUE)
-lims=range(train1$lat_gps)
-grid=seq(from=lims[1], to=lims[2])
-pred=predict(spline.fit1, newdata=list(grid), se=TRUE)
-plot(train1$lat_gps, train1$altitude_barometer)
-lines(grid, pred$fit, lwd=2, col='blue')
-
-
-
+library(gam)
+set.seed(1)
+gam.mse=rep(0,19)
+for (i in 1:19) {
+  sample=data[1:(1396*i),]
+  test=data[(1396*i+1):(1396*(i+1)),]
+  gam.fit=gam(altitude~s(time)+s(temperature), data=sample)
+  gam.pred=predict(gam.fit, test)
+  gam.mse[i]=mean((gam.pred-test$altitude)^2)
+}
+gam.mse # mean mse = 718,245
